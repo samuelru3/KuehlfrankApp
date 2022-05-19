3dca0481ee73fb2b65118c25e196d9fb9263784a
samuelru3 <72190083+samuelru3@users.noreply.github.com>
2022-05-19 23:38:11 +0200

    Commit

--- myFirebase.js
+++ myFirebase.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 console.error('sdgsgd');
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-analytics.js";
+// // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
@@ -15,9 +15,47 @@ const firebaseConfig = {
     measurementId: "G-3BXWGT66J4"
 };
 
+firebase.initializeApp(firebaseConfig);
+
+firebase.auth().onAuthStateChanged(function (user) {
+    var notLoggedIn = document.getElementById('not-logged-in')
+    var loggedIn = document.getElementById('logged-in')
+
+    if (user) {
+        loggedIn.style.display = 'block'
+        notLoggedIn.style.display = 'none'
+    } else {
+        loggedIn.style.display = 'none'
+        notLoggedIn.style.display = 'block'
+    }
+})
+
+
+function login(event) {
+    event.preventDefault()
+    var email = document.getElementById('email').value
+    var password = document.getElementById('password').value
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+        console.log('Error signing in, ', error.message)
+        alert(error.message)
+    }).them(function (user) {
+        if (user) {
+            alert('Welcome back, you are now logged in!')
+        }
+    })
+}
+
+function logout() {
+    firebase.auth().signOut().them(function () {
+
+    }).catch(function (error) {
+
+    });
+}
+
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 // const auth = getAuth(firebaseConfig);
 
 // import {
@@ -38,19 +76,19 @@ const analytics = getAnalytics(app);
 //     console.log(userCredential.user);
 // }
 
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js";
+// import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js";
 
-const auth = getAuth();
-createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        // ...
-    })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-    });
+// const auth = getAuth();
+// createUserWithEmailAndPassword(auth, email, password)
+//     .then((userCredential) => {
+//         // Signed in
+//         const user = userCredential.user;
+//         // ...
+//     })
+//     .catch((error) => {
+//         const errorCode = error.code;
+//         const errorMessage = error.message;
+//         // ..
+//     });
 
-btnLogin.addEventListener("click", loginEmailPassword);
+// btnLogin.addEventListener("click", loginEmailPassword);
